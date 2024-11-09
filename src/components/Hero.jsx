a8d86ffc38d5fdef4ca1659dc4f84f42b3f2006c
Sumedh Badnore <sumedhbadnore@Sumedhs-MacBook-Air.local>
2024-11-09 01:39:08 -0500

    added animation using gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,12 @@ const Hero = () => {
       window.removeEventListener("resize", handleVideoSrcSet);
     };
   });
+
   useGSAP(() => {
-    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
   }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
@@ -45,8 +48,15 @@ const Hero = () => {
         </div>
       </div>
 
-      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          {" "}
+          Buy{" "}
+        </a>
+        <p className="font-normal text-xl"> From $199/month or $999</p>
       </div>
     </section>
   );
